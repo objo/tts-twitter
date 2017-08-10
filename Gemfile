ce808e74c5aa3e5eaa2c4380309a559120afd784
Joe OBrien <joe@objo.com>
2017-08-09 20:11:46 -0400

    adding in user generation

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'paperclip'
 gem 'bootstrap-sass'
 gem 'jquery-rails'
 gem 'font-awesome-rails'
+gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-joe = User.create!(name: 'joe', username: 'objo', location: 'columbus, OH', bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', email: 'joe@objo.com', password: '12345678')
 
-joe.tweets.create!(message: "my first tweet")
-joe.tweets.create!(message: "my second tweet")
-joe.tweets.create!(message: "my third tweet")
-joe.tweets.create!(message: "my fourth tweet")
-joe.tweets.create!(message: "my fifth tweet")
 
-tom = User.create!(name: 'thomas', username: 'timmy', location: 'memphis, tn', bio: 'back in the day ...', email: 'tim@no.com', password: '12345678')
+def create_tweets_for(user)
+  user.tweets.create!(message: Faker::Company.catch_phrase)
+end
 
-tom.tweets.create!(message: 'hello number 1')
-tom.tweets.create!(message: 'hello number 2')
-tom.tweets.create!(message: 'hello number 3')
-tom.tweets.create!(message: 'hello number 4')
-tom.tweets.create!(message: 'hello number 5')
+unless User.find_by(username: 'objo')
+  joe = User.create!(name: 'joe', username: 'objo', location: 'columbus, OH', bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', email: 'joe@objo.com', password: '12345678')
+
+  10.times { create_tweets_for(joe) }
+end
+
+if Tweet.count < 10
+  joe = User.find_by(username: 'objo')
+
+  10.times do |x|
+    user = User.create!(name: Faker::Name.name, username: Faker::Internet.user_name, location: "#{Faker::Address.city}, #{Faker::Address.state_abbr}", email: Faker::Internet.email, password: Faker::Number.number(10))
+    puts "Created user: #{user.username}"
+
+    20.times do |x|
+      create_tweets_for(user)
+    end
+
+    joe.following << user.id
+    puts "joe.following is now #{joe.following}"
+    joe.save!
+  end
+end
